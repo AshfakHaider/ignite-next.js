ac31c0acf7b697beaf9414f69abcef79fc106441
nAzmulHudaa <your_email@example.comasdasasdasdasaadasdsadaasd>
2021-09-25 01:27:38 +0600

    dashboard desiging

---
+++ pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DashboardView from '../src/views/Dashboard/DashboardView';
+
+const dashboard = () => {
+    return (
+        <div>
+            <DashboardView/>
+        </div>
+    );
+};
+
+export default dashboard;
