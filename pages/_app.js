84bf011460d438d2944ffa66a87fa2f8a12c07e3
nAzmulHudaa <your_email@example.comasdasasdasdasaadasdsadaasd>
2021-10-06 15:41:24 +0600

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,7 @@ import PropTypes from "prop-types";
 import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../src/theme/index";
-import "../styles/globals.css";
 import { SSRProvider } from '@react-aria/ssr';
-import 'tailwindcss/tailwind.css';
 import LayoutDefault from '../src/Layout/LayoutDefault';
 
 
