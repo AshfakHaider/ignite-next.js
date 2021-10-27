99dc2bbf7c22f8eb411f297179328eb63f782575
nAzmulHudaa <your_email@example.comasdasasdasdasaadasdsadaasd>
2021-10-28 01:59:10 +0600

    working on font family

--- pages/_document.js
+++ pages/_document.js
@@ -13,37 +13,38 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          <link href="https://fonts.googleapis.com/css2?family=Zen+Antique+Soft&display=swap" as='font' crossOrigin="" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=MonteCarlo&display=swap" as="font"
-            crossOrigin="" rel="stylesheet" />
-          <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-            integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
-            crossOrigin="anonymous"
-          />
-          
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+              <link href="https://fonts.googleapis.com/css2?family=Zen+Antique+Soft&display=swap" rel="stylesheet" />
 
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
+                <link href="https://fonts.googleapis.com/css2?family=MonteCarlo&display=swap" as="font"
+                  crossOrigin="" rel="stylesheet" />
+                <link
+                  rel="stylesheet"
+                  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+                  integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+                  crossOrigin="anonymous"
+                />
 
 
+              </Head>
+              <body>
+                <Main />
+                <NextScript />
 
-          <script async src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-            crossOrigin="anonymous"></script>
-          <script async src="js/jquery-1.11.0.min.js"></script>
-          <script async src="js/bootstrap.min.js"></script>
-          <script async src="js/wow.min.js"></script>
 
-          <script async src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
-          <script async src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-        </body>
-      </Html>
-    );
+
+                <script async src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+                  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+                  crossOrigin="anonymous"></script>
+                <script async src="js/jquery-1.11.0.min.js"></script>
+                <script async src="js/bootstrap.min.js"></script>
+                <script async src="js/wow.min.js"></script>
+
+                <script async src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+                <script async src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+              </body>
+            </Html>
+            );
   }
 }
 
@@ -74,21 +75,21 @@ MyDocument.getInitialProps = async (ctx) => {
 
   // Render app and page and get the context of the page with collected side effects.
   const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
+            const originalRenderPage = ctx.renderPage;
 
   ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+            originalRenderPage({
+              enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
     });
 
-  const initialProps = await Document.getInitialProps(ctx);
+            const initialProps = await Document.getInitialProps(ctx);
 
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
+            return {
+              ...initialProps,
+              // Styles fragment is rendered after the app and page rendering finish.
+              styles: [
+            ...React.Children.toArray(initialProps.styles),
+            sheets.getStyleElement(),
+            ],
   };
 };
